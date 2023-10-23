0ae3ebb2e735cf0f1bcbbab908ff72e09189910c
stephenmurray3 <stephenmurray@live.com>
2023-10-23 15:21:10 +0100

    changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,4 +2,4 @@ export { default as Homepage } from './Homepage';
 export { default as Showspage } from './Showspage';
 export { default as Showpage } from './Showpage';
 export { default as Searchpage } from './Searchpage';
-export { default as NotFoundpage } from './NotFoundpage';
+export { default as NotFoundpage } from './NotFoundPage';
